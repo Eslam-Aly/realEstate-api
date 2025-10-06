e6fcac565809069874ebfd98959af47b7eb182d4
Eslam Aly <eslam.mahmud18@gmail.com>
2025-10-06 20:27:25 +0200

    Create signin page

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import errorHandler from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-const singup = async (req, res, next) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -13,4 +15,26 @@ const singup = async (req, res, next) => {
   }
 };
 
-export default singup;
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const isValidUser = await User.findOne({ email });
+    if (!isValidUser) {
+      return next(errorHandler(404, "user not found!"));
+    }
+    const isPasswordValid = bcryptjs.compareSync(
+      password,
+      isValidUser.password
+    );
+    if (!isPasswordValid) {
+      return next(errorHandler(401, "Invalid email or password!"));
+    }
+    const token = jwt.sign({ id: isValidUser._id }, process.env.JWT_SECRET);
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json({ message: "Signin successful" });
+  } catch (error) {
+    next(error);
+  }
+};
