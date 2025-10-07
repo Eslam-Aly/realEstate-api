c3f8a66b027db7c7a9418d8288c9c722c871c273
Eslam Aly <eslam.mahmud18@gmail.com>
2025-10-07 17:33:30 +0200

    Update header and make profile page private

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,15 @@
 import mangoose from "mongoose";
+import { type } from "os";
 const userSchema = new mangoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    avatar: {
+      type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg",
+    },
   },
   { timestamps: true }
 );
